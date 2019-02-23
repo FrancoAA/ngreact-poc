f2c73b73153b2b82946601a81dd7f681d3e4ac83
Franco <franco.alarcon@gmail.com>
2019-02-23 17:35:50 -0300

    adds component with routing

--- src/components/hello-world.jsx
+++ src/components/hello-world.jsx
@@ -1,9 +1,76 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
-class HelloWorld extends Component {
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
+
+const Topic = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+)
+
+const Topics = ({ match }) => (
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>
+          Rendering with React
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>
+          Components
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>
+          Props v. State
+        </Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.path}/:topicId`} component={Topic}/>
+    <Route exact path={match.path} render={() => (
+      <h3>Please select a topic.</h3>
+    )}/>
+  </div>
+)
+
+export class ComponentWithRouting extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <ul>
+            <li><Link to="/reactjs">Home</Link></li>
+            <li><Link to="/reactjs/about">About</Link></li>
+            <li><Link to="/reactjs/topics">Topics</Link></li>
+          </ul>
+
+          <hr/>
+
+          <Route exact path="/reactjs" component={Home}/>
+          <Route path="/reactjs/about" component={About}/>
+          <Route path="/reactjs/topics" component={Topics}/>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export class HelloWorld extends Component {
   render() {
     return <h1>Hello {this.props.name} {this.props.message}</h1>;
   }
 }
-
-export default HelloWorld;
